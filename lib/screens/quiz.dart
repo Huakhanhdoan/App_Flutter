6c26ba04d23e974e5af15d9aa4efdbe068b60ca0
Huakhanhdoan <21020759@vnu.edu.vn>
2023-11-03 21:27:33 +0700

    fix giao dien

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -14,21 +14,17 @@ class _HoiDapPageState extends State<HoiDapPage> {
   void docCauHoi() async {
     danhSachCauHoi = await Question.docTuJson('./assets/JSON/list_question.json');
     print('Số lượng câu hỏi: ${danhSachCauHoi.length}');
-    setState(() {
-
-    });
-
+    setState(() {});
   }
+
   @override
   void initState() {
     super.initState();
-   docCauHoi();
+    docCauHoi();
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     if (danhSachCauHoi.isEmpty) {
       return const Scaffold(
         body: Center(child: CircularProgressIndicator()),
@@ -38,6 +34,7 @@ class _HoiDapPageState extends State<HoiDapPage> {
     Question cauHoi = danhSachCauHoi[model.cauHienTai];
 
     return Scaffold(
+        backgroundColor: Colors.teal[50],
       appBar: AppBar(
         title: const Text('Tên ứng dụng'),
         actions: <Widget>[
@@ -45,86 +42,81 @@ class _HoiDapPageState extends State<HoiDapPage> {
             padding: const EdgeInsets.all(0),
             child: Center(
               child: Container(
-                width: 100,
+                width: 120,
                 height: 100,
                 decoration: BoxDecoration(
-                  color: Colors.greenAccent, // Đặt màu nền
-                  borderRadius: BorderRadius.circular(30), // Đặt góc bo tròn
+                  color: Colors.green, // Đặt màu nền
+                  borderRadius: BorderRadius.circular(40), // Đặt góc bo tròn
                 ),
                 child: Center(
-                  child: Text(
-                    'Điểm: ${model.diemSo}',
-                    style: TextStyle(fontSize: model.font_size, color: model.color_diem),
+                  child: AnimatedDefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: !model.isCorrect ? 20 : 24, // Kích thước chữ tăng lên khi chọn đúng
+                      color: !model.isCorrect ? Colors.white : Colors.white, // Màu sắc chữ thay đổi khi chọn đúng
+                      fontWeight: FontWeight.bold, // Làm đậm chữ
+                    ),
+                    duration: const Duration(milliseconds: 200), // Thời gian của hiệu ứng
+                    child: Text('Điểm: ${model.diemSo}'),
                   ),
                 ),
               ),
             ),
           )
-
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Câu hỏi ${model.cauHienTai + 1}: ${cauHoi.cauHoi}',
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Câu hỏi ${model.cauHienTai + 1}: ${cauHoi.cauHoi}',
+                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 350.0,
+                  height: 150.0,
+                  child: Image.asset(cauHoi.hinhAnh),
+                ),
+                const SizedBox(height: 20),
+                GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Chia thành 2 cột
+                    crossAxisSpacing: 10.0, // Khoảng cách giữa các cột
+                    mainAxisSpacing: 10.0, // Khoảng cách giữa các hàng
+                      childAspectRatio: 2.0,
+                  ),
+                  shrinkWrap: true, // Sử dụng shrinkWrap để giới hạn kích thước dọc
+                  itemCount: 4,
+                  itemBuilder: (context, i) {
+                    return ListTile(
+                      title: Text(cauHoi.luaChon[i]),
+                      tileColor: model.color_answer[i],
+                      onTap: () {
+                        model.xuLyTraLoi(i + 1, danhSachCauHoi[model.cauHienTai]);
+                        setState(() {});
+                      },
+                    );
+                  },
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            Container(
-              width: 350.0,
-              height: 150.0,
-              child: Image.asset(cauHoi.hinhAnh),
-            ),
-            const SizedBox(height: 20),
-            ListTile(
-              title: Text(cauHoi.luaChon[0]),
-              tileColor: model.c1,
-              onTap: () {
-                model.xuLyTraLoi(1,danhSachCauHoi[model.cauHienTai]);
-                setState(() {}); // Cập nhật trạng thái
+          ),
+          Visibility(
+            visible: model.isChoose,
+            child: ElevatedButton(
+              onPressed: () {
+                model.chuyenCauHoi();
+                setState(() {});
               },
+              child: Text('Câu tiếp theo'),
             ),
-            ListTile(
-              title: Text(cauHoi.luaChon[1]),
-              tileColor: model.c2,
-              onTap: () {
-                model.xuLyTraLoi(2,danhSachCauHoi[model.cauHienTai]);
-                setState(() {}); // Cập nhật trạng thái
-              },
-            ),
-            ListTile(
-              title: Text(cauHoi.luaChon[2]),
-              tileColor: model.c3,
-              onTap: () {
-                model.xuLyTraLoi(3,danhSachCauHoi[model.cauHienTai]);
-                setState(() {}); // Cập nhật trạng thái
-              },
-            ),
-            ListTile(
-              title: Text(cauHoi.luaChon[3]),
-              tileColor: model.c4,
-              onTap: () {
-                model.xuLyTraLoi(4,danhSachCauHoi[model.cauHienTai]);
-                setState(() {}); // Cập nhật trạng thái
-              },
-            ),
-            Visibility(
-              visible: model.isChoose, // Nút sẽ hiển thị khi daChonCauHoi là true
-              child: ElevatedButton(
-                onPressed : () {
-                  model.chuyenCauHoi();// Hàm này sẽ chuyển sang câu hỏi tiếp theo
-                  setState(() {
-
-                  });
-                },
-                child: Text('Câu tiếp theo'),
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
